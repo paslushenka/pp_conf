1357fca0087600fc64f9ee5d352434196f26cadc
Aliaksandr Paslushanka <60079447+paslushenka@users.noreply.github.com>
2020-02-03 21:37:54 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,9 +5,15 @@ node 'master.puppet' {
 }
 
 node 'slave1.puppet' {
+  file { '/root/README':
+    ensure => absent,
+    }
 }
 
 node 'slave2.puppet' {
+  file { '/root/README':
+    ensure => absent,
+    }
 }
 
 node 'mineserver.puppet' {
