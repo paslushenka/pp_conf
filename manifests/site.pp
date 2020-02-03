55181d89454e5b0ffb969351b7240cf6393f1bc4
Aliaksandr Paslushanka <60079447+paslushenka@users.noreply.github.com>
2020-02-04 02:43:10 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,16 +5,35 @@ node 'master.puppet' {
 }
 
 node 'slave1.puppet' {
-  file { '/root/README':
-    ensure => absent,
-    }
+   class { 'apache': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/web1.org/html/index.html':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/paslushenka/devops-hometasks/master/task-04/files/index.html',
+      replace => false,
+      }
 }
 
 node 'slave2.puppet' {
-  file { '/root/README':
-    ensure => absent,
-    }
+   class { 'apache::mod::php': }
+   
+   class { 'php': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/index.php':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/paslushenka/devops-hometasks/master/task-04/files/index.php',
+      replace => false,
+      }
 }
+
 
 node 'mineserver.puppet' {
 }
